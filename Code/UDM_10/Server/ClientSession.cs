6761a15dd05b916fb926aad148426c7709fea274
Trần Hữu Nam <namth0411@ut.edu.vn>
2026-08-01 11:46:15 +0700

    Add files via upload

---
+++ Code/UDM_10/Server/ClientSession.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Sockets;
+
+class ClientSession
+{
+    private readonly TcpClient _client;
+
+    public ClientSession(TcpClient client)
+    {
+        _client = client;
+    }
+
+
+    public async Task RunAsync()
+    {
+        Console.WriteLine("Client session started.");
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("Client session ended.");
+
+        _client.Close();
+    }
+}
---
+++ Code/UDM_10/Server/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        await RunAsync();
+    }
+
+
+    static async Task RunAsync()
+    {
+        TcpListener listener =
+            new TcpListener(IPAddress.Any, 5000);
+
+        listener.Start();
+
+        Console.WriteLine("Server started...");
+        Console.WriteLine("Waiting for client...");
+
+
+        while (true)
+        {
+            TcpClient client =
+                await listener.AcceptTcpClientAsync();
+            
+            Console.WriteLine("Client connected");
+
+            ClientSession session =
+                new ClientSession(client);
+
+            _ = Task.Run(() => session.RunAsync());
+        }
+    }
+}
