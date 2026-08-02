bee50f201c5c3b7f91c4d68b2fcbc363775d557c
phu <hothienphufpt@gmail.com>
2026-08-02 22:35:50 +0700

    feat: add NetworkClient.cs

---
+++ Services/NetworkClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDM_10.Client.services
+{
+    public class NetworkClient
+    {
+        private TcpClient _client;
+        private NetworkStream _stream;
+
+        public async Task<bool> ConnectAsync(string ipAddress, int port)
+        {
+            try
+            {
+                _client = new TcpClient();
+                await _client.ConnectAsync(ipAddress, port);
+                _stream = _client.GetStream();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
