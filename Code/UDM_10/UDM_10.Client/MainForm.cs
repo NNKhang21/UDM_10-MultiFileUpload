a0b1c376562b9295ab8ed6a1c249af13f7486654
NNKhang21 <kn537190@gmail.com>
2026-08-22 23:05:17 +0700

    Cap nhat

--- Code/UDM_10/UDM_10.Client/MainForm.cs
+++ Code/UDM_10/UDM_10.Client/MainForm.cs
@@ -50,14 +50,14 @@ namespace UDM_10.Client
             gridFiles.CellBorderStyle = DataGridViewCellBorderStyle.Single;
             gridFiles.GridColor = Color.FromArgb(200, 205, 210);
             gridFiles.RowTemplate.Height = 42;
-            gridFiles.BackgroundColor = Color.White;
+            gridFiles.BackgroundColor = Color.FloralWhite;
             gridFiles.Font = new Font("Segoe UI", 9.5f);
 
-            gridFiles.ColumnHeadersDefaultCellStyle.BackColor = Color.White;
+            gridFiles.ColumnHeadersDefaultCellStyle.BackColor = Color.FloralWhite;
             gridFiles.ColumnHeadersDefaultCellStyle.ForeColor = TextDarkColor;
             gridFiles.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI", 10f, FontStyle.Bold);
             gridFiles.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
-            gridFiles.ColumnHeadersDefaultCellStyle.SelectionBackColor = Color.White;   
+            gridFiles.ColumnHeadersDefaultCellStyle.SelectionBackColor = Color.FloralWhite;   
             gridFiles.ColumnHeadersDefaultCellStyle.SelectionForeColor = TextDarkColor;      
             gridFiles.ColumnHeadersHeight = 40;
             gridFiles.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
@@ -170,7 +170,7 @@ namespace UDM_10.Client
                 UpdateFooter();
             };
 
-            lblConcurrencyInfo.Text = $"⚡Đồng thời tối đa: {UploadManager.MaxConcurrentUploads} file";
+            lblConcurrencyInfo.Text = $"⚡ Đồng thời tối đa: {UploadManager.MaxConcurrentUploads} file";
             UpdateFooter();
         }
 
