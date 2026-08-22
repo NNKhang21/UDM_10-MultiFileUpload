6d190b9bd7e59b80274963c4fa001cb051b574b9
NNKhang21 <kn537190@gmail.com>
2026-08-22 23:07:04 +0700

    Cap nhan nen trang

--- Code/UDM_10/UDM_10.Client/MainForm.cs
+++ Code/UDM_10/UDM_10.Client/MainForm.cs
@@ -50,14 +50,14 @@ namespace UDM_10.Client
             gridFiles.CellBorderStyle = DataGridViewCellBorderStyle.Single;
             gridFiles.GridColor = Color.FromArgb(200, 205, 210);
             gridFiles.RowTemplate.Height = 42;
-            gridFiles.BackgroundColor = Color.FloralWhite;
+            gridFiles.BackgroundColor = Color.White;
             gridFiles.Font = new Font("Segoe UI", 9.5f);
 
-            gridFiles.ColumnHeadersDefaultCellStyle.BackColor = Color.FloralWhite;
+            gridFiles.ColumnHeadersDefaultCellStyle.BackColor = Color.White;
             gridFiles.ColumnHeadersDefaultCellStyle.ForeColor = TextDarkColor;
             gridFiles.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI", 10f, FontStyle.Bold);
             gridFiles.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
-            gridFiles.ColumnHeadersDefaultCellStyle.SelectionBackColor = Color.FloralWhite;   
+            gridFiles.ColumnHeadersDefaultCellStyle.SelectionBackColor = Color.White;   
             gridFiles.ColumnHeadersDefaultCellStyle.SelectionForeColor = TextDarkColor;      
             gridFiles.ColumnHeadersHeight = 40;
             gridFiles.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
